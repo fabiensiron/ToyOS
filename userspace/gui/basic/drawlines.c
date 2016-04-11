bc44ce4dd9abbfdd70324053b4136e59b4b919c8
Jason Lu <jasonbangbang@gmail.com>
2016-04-11 14:07:43 -0500

    Update copyright headers from ToaruOS to ToyOS
    [ci skip]

--- userspace/gui/basic/drawlines.c
+++ userspace/gui/basic/drawlines.c
@@ -1,4 +1,4 @@
-/* This file is part of ToaruOS and is released under the terms
+/* This file is part of ToyOS and is released under the terms
  * of the NCSA / University of Illinois License - see LICENSE.md
  * Copyright (C) 2013-2014 Kevin Lange
  */
--- userspace/net/fetch.c
+++ userspace/net/fetch.c
@@ -1,5 +1,5 @@
 /* vim: ts=4 sw=4 noexpandtab
- * This file is part of ToaruOS and is released under the terms
+ * This file is part of ToyOS and is released under the terms
  * of the NCSA / University of Illinois License - see LICENSE.md
  * Copyright (C) 2015 Kevin Lange
  *
