af2a108616cb74ef4c5ead0ebcb5198e10fff6c1
Jason Lu <jasonbangbang@gmail.com>
2016-04-11 09:16:28 -0500

    Use unsigned char instead of unsigned int

--- userspace/extra/toaru_logo.h
+++ userspace/extra/toaru_logo.h
@@ -4,7 +4,7 @@ static const struct {
   unsigned int  	 width;
   unsigned int   	 height;
   unsigned int   	 bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */
-  unsigned int  	 pixel_data[39 * 39 * 4 + 1];
+  unsigned char  	 pixel_data[39 * 39 * 4 + 1];
 } gimp_image = {
   39, 39, 4,
   "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
