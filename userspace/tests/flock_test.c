de914c52d15f845719a8b05af5f9b6e3e21551c0
Fabien Siron <fabien.siron@epita.fr>
2016-04-19 10:48:32 +0000

    flock: test file

---
+++ userspace/tests/flock_test.c
@@ -0,0 +1,33 @@
+#include <sys/file.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <syscall.h>
+
+int
+main(int argc, char *argv[]) {
+	int fd = open("FLOCK_TEST", O_CREAT);
+
+	int r = flock(fd, LOCK_EX | LOCK_NB);
+
+	if (r == -1) {
+		printf("Waiting for free external unlock...\n");
+		while(flock(fd, LOCK_EX | LOCK_NB) == -1);
+		printf("Resource unlocked\n");
+	}
+
+	printf("wait...\n");
+
+	sleep(5);
+
+	printf("unlock...\n");
+
+	flock(fd, LOCK_UN | LOCK_NB);
+
+	printf("done!\n");
+
+	close(fd);
+
+	return 0;
+}
